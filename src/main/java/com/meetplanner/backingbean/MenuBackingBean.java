406eee803c9749ce2c57722f8cc689fc8a226a81
Lakmal Dasanayake <dasanayaka70@gmail.com>
2015-07-15 16:54:22 +0530

    add upload page

---
+++ src/main/java/com/meetplanner/backingbean/MenuBackingBean.java
@@ -0,0 +1,18 @@
+package com.meetplanner.backingbean;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ * @author lakmal.d
+ */
+
+@ManagedBean(name="menuBean")
+@ApplicationScoped
+public class MenuBackingBean {
+
+	public String goToFileUpload(){
+		System.out.println("in goToFileUpload");
+		return "pages/fileUpload";
+	}
+}
