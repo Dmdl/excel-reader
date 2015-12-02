f96f2ef2df9bda2e30302ca317deb25a745e347e
Lakmal Dasanayake <dasanayaka70@gmail.com>
2015-12-02 15:36:56 +0530

    change theme dynamically

--- src/main/java/com/meetplanner/backingbean/ThemeSwitcherView.java
+++ src/main/java/com/meetplanner/backingbean/ThemeSwitcherView.java
@@ -1,17 +1,25 @@
 package com.meetplanner.backingbean;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import org.primefaces.component.themeswitcher.ThemeSwitcher;
 
 import com.meetplanner.dto.Theme;
 
 @ManagedBean
-public class ThemeSwitcherView {
+@SessionScoped
+public class ThemeSwitcherView implements Serializable{
 
+	private static final long serialVersionUID = 1L;
 	private List<Theme> themes;
+	private String theme = "south-street";
 
 	@ManagedProperty("#{themeService}")
 	private ThemeService service;
@@ -21,6 +29,10 @@ public class ThemeSwitcherView {
 		themes = service.getThemes();
 	}
 
+	public void saveTheme(AjaxBehaviorEvent ajax) {
+		setTheme((String) ((ThemeSwitcher) ajax.getSource()).getValue());
+	}
+	
 	public List<Theme> getThemes() {
 		return themes;
 	}
@@ -28,4 +40,17 @@ public class ThemeSwitcherView {
 	public void setService(ThemeService service) {
 		this.service = service;
 	}
+
+	public String getTheme() {
+		return theme;
+	}
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+	public void setThemes(List<Theme> themes) {
+		this.themes = themes;
+	}
+	
 }
