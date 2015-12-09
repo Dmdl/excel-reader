0ada2424982f95583fcc9a97fef8234081f116b3
Lakmal Dasanayake <dasanayaka70@gmail.com>
2015-12-09 23:44:57 +0530

    issue fixed - group not show

--- src/main/java/com/meetplanner/dao/CommonDaoNamedImpl.java
+++ src/main/java/com/meetplanner/dao/CommonDaoNamedImpl.java
@@ -65,7 +65,7 @@ public class CommonDaoNamedImpl extends NamedParameterJdbcDaoSupport implements 
 	public List<Athlete> serachAthleteByBibOrName(String bib, String name) {
 		List<Athlete> results = null;
 		try{
-			String sql = "SELECT * FROM athlete WHERE athlete.name LIKE :pname";
+			String sql = "SELECT athlete.id AS athlete_id,athlete.name AS athlete_name,date_of_birth,group_id,nic,gender,age_group_id,bib,groups.name AS group_name FROM athlete LEFT JOIN groups ON athlete.group_id=groups.id WHERE athlete.name LIKE :pname";
 			String quName = "%" + name + "%";
 			Map<String, Object> parameters = new HashMap<String, Object>();
 			parameters.put("pname", quName);
